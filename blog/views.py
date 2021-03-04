e0208330b2147a1d743eea686accd9ddac6fb731
Antonio <Tenundor@gmail.com>
2021-03-04 20:58:40 +0300

    Prefetch posts count for tags

--- blog/views.py
+++ blog/views.py
@@ -30,7 +30,7 @@ def serialize_post_optimised(post):
         "image_url": post.image.url if post.image else None,
         "published_at": post.published_at,
         "slug": post.slug,
-        "tags": [serialize_tag(tag) for tag in post.tags.all()],
+        "tags": [serialize_tag_optimised(tag) for tag in post.tags.all()],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -42,19 +42,29 @@ def serialize_tag(tag):
     }
 
 
+def serialize_tag_optimised(tag):
+    return {
+        'title': tag.title,
+        'posts_with_tag': tag.posts.count(),
+    }
+
+
 def index(request):
     most_popular_posts = Post.objects.popular() \
-                                     .prefetch_related('author')[:5] \
+                                     .prefetch_related('author') \
+                                     .prefetch_related('tags__posts')[:5] \
                                      .fetch_with_comments_count()
     most_fresh_posts = Post.objects.all().order_by('-published_at') \
-                                         .prefetch_related('author')[:5] \
+                                         .prefetch_related('author') \
+                                         .prefetch_related('tags__posts')[:5] \
                                          .fetch_with_comments_count()
-    most_popular_tags = Tag.objects.popular()[:5]
+    most_popular_tags = Tag.objects.popular() \
+                           .prefetch_related('posts')[:5]
 
     context = {
         'most_popular_posts': [serialize_post_optimised(post) for post in most_popular_posts],
         'page_posts': [serialize_post_optimised(post) for post in most_fresh_posts],
-        'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
+        'popular_tags': [serialize_tag_optimised(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
 
