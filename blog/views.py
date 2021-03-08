781e4f84b80e6f424fc830792ceee13e47374a79
Antonio <Tenundor@gmail.com>
2021-03-08 21:51:58 +0300

    Remove unused functions

--- blog/views.py
+++ blog/views.py
@@ -4,25 +4,7 @@ from django.db.models import Prefetch
 from blog.models import Comment, Post, Tag
 
 
-def get_related_posts_count(tag):
-    return tag.posts.count()
-
-
-def serialize_post(post):
-    return {
-        "title": post.title,
-        "teaser_text": post.text[:200],
-        "author": post.author.username,
-        "comments_amount": len(Comment.objects.filter(post=post)),
-        "image_url": post.image.url if post.image else None,
-        "published_at": post.published_at,
-        "slug": post.slug,
-        "tags": [serialize_tag(tag) for tag in post.tags.all()],
-        "first_tag_title": post.tags.all()[0].title,
-    }
-
-
-def serialize_post_optimised(post, serialized_comments=None, is_teaser=True):
+def serialize_post(post, serialized_comments=None, is_teaser=True):
     serialized_post = {
         "title": post.title,
         "author": post.author.username,
@@ -48,13 +30,6 @@ def serialize_post_optimised(post, serialized_comments=None, is_teaser=True):
 
 
 def serialize_tag(tag):
-    return {
-        "title": tag.title,
-        "posts_with_tag": len(Post.objects.filter(tags=tag)),
-    }
-
-
-def serialize_tag_optimised(tag):
     return {
         "title": tag.title,
         "posts_with_tag": tag.num_posts,
